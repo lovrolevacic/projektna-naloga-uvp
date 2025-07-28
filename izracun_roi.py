7be460631acef3ff357beadebd6d78ab56a67fa0
lovrolevacic <168608486+lovrolevacic@users.noreply.github.com>
2025-07-28 15:32:37 +0200

    Posodobitev formule

--- izracun_roi.py
+++ izracun_roi.py
@@ -1,5 +1,5 @@
 def izracunaj_roi(budget, revenue):
     """Izračunaj ROI – Return on Investment."""
     if budget and budget > 0:
-        return ((revenue - budget) / budget) * 100 # Množenje s 100, da dobimo rezultat v procentih
+        return (revenue - budget) / budget
     return None
